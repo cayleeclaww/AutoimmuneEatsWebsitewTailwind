6ab372f9740910d65424a304317f02a57d827764
Caylee Claww <ninjahead@gmail.com>
2023-11-10 15:37:53 -0500

    updating FourCardSpread title so smaller for mobile view

--- components/FourCardSpread.jsx
+++ components/FourCardSpread.jsx
@@ -6,7 +6,7 @@ export default function FourCardSpread(props) {
     <>
       <div className="w-full mt-24">
         <p
-          className="text-6xl pl-6 pb-2 lowercase"
+          className="text-4xl md:text-6xl pl-6 pb-2 lowercase"
           style={{ textShadow: "3px 3px 3px #99CED3" }}
         >
           {props.children}
