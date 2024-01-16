4e752a84b2242ea985840a29483796196fdbab96
Caylee Claww <ninjahead@gmail.com>
2024-01-16 08:00:27 -0500

    building Top Supps page without <script>

--- components/EmailPrompt.jsx
+++ components/EmailPrompt.jsx
@@ -24,29 +24,19 @@ export default function EmailPrompt() {
             method="post"
             id="mc-embedded-subscribe-form"
             name="mc-embedded-subscribe-form"
-            class="validate"
+            className="validate"
             target="_blank"
             novalidate=""
           >
             <div id="mc_embed_signup_scroll" className="pt-6">
-              <h2 className="text-center ">Join our email list!</h2>
-
-              <div class="mc-field-group">
+              <h2 className="text-3xl font-bold my-4">Join our email list!</h2>
+              <div className="mc-field-group">
                 <label for="mce-EMAIL">
-                  Email Address 
-                  {/* <span class="asterisk">*</span> */}
-                  {/* <input
-                    type="email"
-                    name="EMAIL"
-                    class="required email"
-                    id="mce-EMAIL"
-                    required=""
-                    value=""
-                  /> */}
+                  Email Address
                   <input
                     type="email"
                     name="EMAIL"
-                    class="required email"
+                    className="required email"
                     id="mce-EMAIL"
                     required=""
                     value={email}
@@ -54,21 +44,18 @@ export default function EmailPrompt() {
                     onChange={(e) => setEmail(e.target.value)}
                   />
                 </label>
-                {/* <div class="indicates-required">
-                  <span class="asterisk">*</span> indicates required
-                </div> */}
               </div>
               <div hidden="">
-                <input type="hidden" name="tags" value="2270440" />
+                <input type="hidden" name="tags" defaultValue="2270440" />
               </div>
-              <div id="mce-responses" class="clear foot">
+              <div id="mce-responses" className="clear foot">
                 <div
-                  class="response"
+                  className="response"
                   id="mce-error-response"
                   style={{ display: "none" }}
                 ></div>
                 <div
-                  class="response"
+                  className="response"
                   id="mce-success-response"
                   style={{ display: "none" }}
                 ></div>
@@ -83,46 +70,18 @@ export default function EmailPrompt() {
                   type="text"
                   name="b_ed3887d0c66e0bebf95e5adf4_db78bcafa9"
                   tabindex="-1"
-                  value=""
+                  defaultValue=""
                 />
               </div>
-              <div class="optionalParent">
-                <div class="clear foot">
+              <div className="optionalParent">
+                <div className="clear foot">
                   <input
                     type="submit"
                     name="subscribe"
                     id="mc-embedded-subscribe"
-                    class="button"
-                    value="Subscribe"
+                    className="button"
+                    value="Sign up!"
                   />
-                  <p style={{ margin: "0px auto" }}>
-                    <a
-                      href="http://eepurl.com/iHFVvk"
-                      title="Mailchimp - email marketing made easy and fun"
-                    >
-                      <span
-                        style={{
-                          display: "inline-block",
-                          backgroundColor: "transparent",
-                          borderRadius: "4px",
-                        }}
-                      >
-                        <img
-                          class="refferal_badge"
-                          src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg"
-                          alt="Intuit Mailchimp"
-                          style={{
-                            width: "220px",
-                            height: "40px",
-                            display: "flex",
-                            padding: "2px 0px",
-                            justifyContent: "center",
-                            alignItems: "center",
-                          }}
-                        />
-                      </span>
-                    </a>
-                  </p>
                 </div>
               </div>
             </div>
@@ -131,173 +90,4 @@ export default function EmailPrompt() {
       </div>
     </div>
   );
-}
-
-{
-  /* //   const [email, setEmail] = useState("");
-//   const [loading, setLoading] = useState(false);
-
-//   const onSubmit = (e) => { 
-//     e.preventDefault();
-//     const url =
-//       "https://eatyerveggies.us4.list-manage.com/subscribe/post-json?u=ed3887d0c66e0bebf95e5adf4&amp;id=db78bcafa9&amp;f_id=00812fe9f0";
-//     jsonp(`${url}&EMAIL=${email}`, { param: "c" }, (_, data) => {
-//       const { msg, result } = data;
-//       // do something with response
-//       alert(msg);
-//     });
-//   };
-
-//   return (
-
-//     <div className="w-full bg-turquoise h-80 grid justify-items-center">
-//       <p className="text-center pt-16 text-xl">Join our email list!</p>
-//       <form action="" method="get" className="text-center ">
-//         <div onSubmit={onSubmit}>
-//           <label for="email" onChange={(e) => setEmail(e.target.value)}>
-//             Email:{" "}
-//           </label>
-//           <br></br>
-//           <input
-//             type="email"
-//             name="EMAIL"
-//             // class="required email"
-//             id="mce-EMAIL"
-//             className="w-64 rounded h-8"
-//             required
-//           />
-//         </div>
-//         <div>
-//           <input
-//             type="submit"
-//             value="Subscribe!"
-//             className="text-l p-2 m-3 bg-white uppercase rounded-md justify-center w-[95%] text-black hover:font-bold"
-//             isLoading={loading}
-//           />
-//         </div>
-//       </form>
-//       {/* action="https://eatyerveggies.us4.list-manage.com/subscribe/post-json?u=ed3887d0c66e0bebf95e5adf4&amp;id=db78bcafa9&amp;f_id=00812fe9f0" */
-}
-//       {/* <input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value="" />
-//     </div>*/}
-
-{
-  /* 
-
-    //   <div id="mc_embed_shell">
-    //     <link 
-    //       href="//cdn-images.mailchimp.com/embedcode/classic-061523.css"
-    //       rel="stylesheet"
-    //       type="text/css"
-    //     />
-    //     <style type="text/css">
-    //       {/* #mc_embed_signup{background:"#fff", clear:"left", width: "600px"} */
-}
-//       {/* /* Add your own Mailchimp form style overrides in your site stylesheet
-//       or in this style block. We recommend moving this block and the
-//       preceding CSS link to the HEAD of your HTML file. */}
-{
-  /* //     </style>
-    //     <div id="mc_embed_signup">
-    //       <form 
-    //         action="https://eatyerveggies.us4.list-manage.com/subscribe/post?u=ed3887d0c66e0bebf95e5adf4&amp;id=db78bcafa9&amp;f_id=00812fe9f0"
-    //         method="post"
-    //         id="mc-embedded-subscribe-form"
-    //         name="mc-embedded-subscribe-form"
-    //         class="validate"
-    //         target="_self"
-    //         novalidate=""
-    //       >
-    //         <div id="mc_embed_signup_scroll" className="pt-8">
-    //           <h2 className="grid justify-items-center">
-    //             Join our email list!
-    //           </h2>
-    //           <div class="indicates-required">
-    //             <span class="asterisk">*</span> indicates required
-    //           </div>
-    //           <div class="mc-field-group">
-    //             <label for="mce-EMAIL">
-    //               Email Address <span class="asterisk">*</span>
-    //             </label>
-    //             <input
-    //               type="email"
-    //               name="EMAIL"
-    //               class="required email"
-    //               id="mce-EMAIL"
-    //               required=""
-    //               value=""
-    //             />
-    //             <span
-    //               id="mce-EMAIL-HELPERTEXT"
-    //               class="helper_text"
-    //               className="hidden"
-    //             ></span>
-    //           </div>
-    //           <div hidden="">
-    //             <input type="hidden" name="tags" value="2270440" />
-    //           </div>
-    //           <div id="mce-responses" class="clear foot">
-    //             <div
-    //               class="response"
-    //               id="mce-error-response"
-    //               className="hidden"
-    //             ></div>
-    //             <div
-    //               class="response"
-    //               id="mce-success-response"
-    //               className="hidden"
-    //             ></div>
-    //           </div>
-    //           <div
-    //             aria-hidden="true"
-    //             // style="position: absolute; left: -5000px;"
-    //             className="absolute -ml-96"
-    //           >
-    //             {/* /* real people should not fill this in and expect good things
-    //               - do not remove this or risk form bot signups */
-}
-{
-  /* //             <input 
-    //               type="text"
-    //               name="b_ed3887d0c66e0bebf95e5adf4_db78bcafa9"
-    //               tabindex="-1"
-    //               value=""
-    //             />
-    //           </div>
-    //           <div class="optionalParent">
-    //             <div class="clear foot">
-    //               <input
-    //                 type="submit"
-    //                 name="subscribe"
-    //                 id="mc-embedded-subscribe"
-    //                 class="button"
-    //                 value="Subscribe"
-    //               />
-    //               <p
-    //                 // style="margin: 0px auto;"
-    //                 className="mx-auto my-0"
-    //               >
-    //                 <a
-    //                     href="http://eepurl.com/iHFVvk"
-    //                     title="Mailchimp - email marketing made easy and fun"
-    //                   >
-    //                 <span
-    //                       // style="display: inline-block; background-color: transparent; border-radius: 4px;"
-    //                       className="inline-block rounded"
-    //                     >
-    //                 {/* <img
-    //                         class="refferal_badge"
-    //                         src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg"
-    //                         alt="Intuit Mailchimp"
-    //                         // style="width: 220px; height: 40px; display: flex; padding: 2px 0px; justify-content: center; align-items: center;"
-    //                       /> 
-    //                 </span>
-    //                 </a>
-    //               </p>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </form>
-    //     </div>
-    //   </div>*/
 }
--- src/app/(categories)/nutrition-and-supplements/top-supplements/page.js
+++ src/app/(categories)/nutrition-and-supplements/top-supplements/page.js
@@ -1,36 +1,124 @@
-// "use client"
 import TitleRectangleTurquoise from "../../../../../components/TitleRectangleTurquoise";
+import Image from "next/image";
 import Script from "next/script";
+import FullscriptSupplement from "../../../../../components/FullscriptSupplement";
+import UseScript from "../../../../../components/UseScript";
+import Link from "next/link";
+import vitamin_D_thorne_supplement_image from "../../../../../public/supplements/vitamin_D_thorne_supplement_image.png";
 
+// FOR LATER: figure out how to get supplement in flexbox
 
+// us.fullscript.com/o/catalog/products/U3ByZWU6OlByb2R1Y3QtODkwMjA=
+// us.fullscript.com/o/catalog/products/U3ByZWU6OlByb2R1Y3QtODkwMjA=
+// us.fullscript.com/o/catalog/products/U3ByZWU6OlByb2R1Y3QtODQwNDQ=
+//us.fullscript.com/welcome/cclaynutrition?product_id=84044&signup_source=product_cards
+//us.fullscript.com/welcome/autoimmunenutrition
 
-// FOR LATER: figure out how to get supplement in flexbox 
-
-
-
-export default function NutAndSuppsPage() {
+export default function TopSuppsPage() {
   return (
     <>
       {/* turquoise category title rectangle - desktop & mobile */}
       <TitleRectangleTurquoise>
         <p>Top Supplements</p>
       </TitleRectangleTurquoise>
-      {/* <body
-      style={{display: "flex"}}
-      > */}
+      {/* banner to Fullscript dispensary */}
+      <a
+        className="flex justify-center"
+        href="https://us.fullscript.com/welcome/autoimmunenutrition?utm_medium=webreferral&utm_source=other&utm_campaign=abmwebbuttons_light_728x90.svg&signup_source=website_buttons"
+        target="_top"
+      >
+        <img
+          src="https://assets.fullscript.com/buttons/light_728x90.svg"
+          alt="Order supplements through my Fullscript store."
+        />
+      </a>
+      {/* all supplements */}
+      <div className="grid md:w-1/2 mx-auto py-10 divide-y divide-solid">
+        {/* vitamin D supplements - use BELOW code to make component!! */}
+        <div className="grid md:grid-cols-[30%_70%] px-4 py-10">
+          <Link
+            href="https://us.fullscript.com/welcome/autoimmunenutrition/store-start"
+            target="_blank"
+            className="my-auto mx-auto "
+          >
+            <Image
+              src={vitamin_D_thorne_supplement_image}
+              width={182}
+              height={370}
+              className=""
+            ></Image>
+          </Link>
+          <div className="pt-6 grid p-2">
+            <h2 className="text-xl md:text-3xl font-bold mx-auto text-center">
+              Vitamin D Liquid
+            </h2>
+            <p className="mx-auto text-center">
+              Vitamin D is essential to proper immune system functioning.
+              Deficiency in vitamin D may make autoimmune disease worse, &
+              increases the risk of infection, which also can worsen autoimmune
+              disease.<br></br>
+              <br></br>
+              Vitamin D also helps promote healthy bones & maintain adequate
+              levels of calcium across the body (
+              <Link href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3166406/">
+                PubMed
+              </Link>
+              ).
+            </p>
+          </div>
+        </div>
+        {/* vitamin D supplements - USE THSI CODE!! */}
+        <div className="grid md:grid-cols-[30%_70%] px-4 py-10">
+          <Link
+            href="https://us.fullscript.com/welcome/autoimmunenutrition/store-start"
+            target="_blank"
+            className="my-auto mx-auto "
+          >
+            <Image
+              src={vitamin_D_thorne_supplement_image}
+              width={182}
+              height={370}
+            ></Image>
+          </Link>
+          <div className="pt-4 grid p-2 mx-auto place-content-start">
+            <h2 className="text-xl md:text-3xl font-bold mx-auto text-center my-4">
+              Vitamin D Liquid
+            </h2>
+            <p className="text-center">
+              Vitamin D is essential to proper immune system functioning.
+              Deficiency in vitamin D may make autoimmune disease worse, &
+              increases the risk of infection, which also can worsen autoimmune
+              disease.<br></br>
+              <br></br>
+              Vitamin D also helps promote healthy bones & maintain adequate
+              levels of calcium across the body (
+              <Link href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3166406/">
+                PubMed
+              </Link>
+              ).
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* <Link href="https://us.fullscript.com/plans/cclaynutrition-vitamin-d">
+        Vitamin D
+      </Link> */}
+      {/* <UseScript/>
+      UseScript('"product_id":"89020","store_slug":"cclaynutrition","return":"product_card"') */}
+      {/* <FullscriptSupplement product_id={89020}></FullscriptSupplement> */}
       {/* <div className="flex"> */}
       {/* NOW Liquid Vitamin D-3 */}
-      {/* <Script
+      {/* <script
         src="//us.fullscript.com/oembed/embed.js"
-        data-fs='{"product_id":"89020","store_slug":"cclaynutrition","return":"product_card"}' */}
-      {/* // strategy="beforeInteractive" */}
-      {/* ></Script> */}
+        data-fs='{"product_id":"76524","store_slug":"autoimmunenutrition","return":"product_card"}'
+      ></script> */}
+
       {/* Seeking Health Multivitamin One MF */}
-      {/* <Script
+      {/* <script
         src="//us.fullscript.com/oembed/embed.js"
         data-fs='{"product_id":"84044","store_slug":"cclaynutrition","return":"product_card"}'
         defer
-      ></Script> */}
+      ></script> */}
       {/* NOW Omega-3 */}
       {/* <script
         src="//us.fullscript.com/oembed/embed.js"
@@ -62,7 +150,6 @@ export default function NutAndSuppsPage() {
         defer
       ></script> */}
       {/* </div> */}
-      {/* </body> */}
     </>
   );
 }
