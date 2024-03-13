1569a29bf2eacbed213e0e0000f77604993c792f
Caylee Claww <ninjahead@gmail.com>
2024-03-13 15:44:06 -0400

    Adding second button to BuyBookNow component for Bookshop.org link

--- components/BookBuyNow.jsx
+++ components/BookBuyNow.jsx
@@ -8,7 +8,7 @@ const BookBuyNow = () => {
       {/* desktop responsiveness */}
       <div className="w-full flex justify-center py-16 items-center">
         <div className="hidden md:grid m-6 border-solid border-2 border-grey h-fit max-h-90 max-w-md items-center mt-14 bg-white">
-          <p className="text-3xl py-4 px-2 text-bold text-center">
+          <p className="text-3xl py-6 px-4 text-bold text-center">
             Gain Control Over Your Psoriasis
           </p>
           <p className="text-l px-4 pb-4">
@@ -20,11 +20,22 @@ const BookBuyNow = () => {
             by attempting to follow some unproven highly restrictive psoriasis
             diet.
           </p>
-          <Link href="https://amzn.to/3xSaKFJ" target="_blank">
-            <button className="text-l p-2 m-3 bg-turquoise uppercase rounded-md justify-center w-[95%] text-black hover:font-bold">
-              buy here
-            </button>
-          </Link>
+          <div className="grid grid-cols-2 gap-3">
+            <Link
+              href="https://bookshop.org/a/102444/9781735150413"
+              target="_blank"
+              className="flex justify-center items-center text-center text-l p-2 my-4 ml-6 bg-turquoise uppercase rounded-md text-black hover:font-bold"
+            >
+              buy on Bookshop.org
+            </Link>
+            <Link
+              href="https://amzn.to/3xSaKFJ"
+              target="_blank"
+              className="flex justify-center items-center text-center text-l p-2 my-4 mr-6 bg-turquoise uppercase rounded-md text-black hover:font-bold"
+            >
+              buy on Amazon
+            </Link>
+          </div>
           <p className="text-sm italic text-center mx-3 mb-3 text-gray-400">
             These are affiliate links, which keep this informational website
             free of charge. Bookshop.org supports our work the most.
@@ -57,11 +68,22 @@ const BookBuyNow = () => {
           Make peace with your body by learning your psoriasis triggers, not by
           attempting to follow some unproven highly restrictive psoriasis diet.
         </p>
-        <Link href="https://amzn.to/3xSaKFJ" target="_blank">
-          <button className="text-l p-2 m-3 w-60 bg-turquoise uppercase rounded-md justify-center text-black hover:font-bold">
-            buy here
-          </button>
-        </Link>
+        <div className="grid grid-cols-2 gap-3 w-full">
+          <Link
+            href="https://bookshop.org/a/102444/9781735150413"
+            target="_blank"
+            className="flex justify-center items-center text-center px-auto text-l p-2 my-4 ml-6 bg-turquoise uppercase rounded-md text-black hover:font-bold"
+          >
+            buy on Bookshop.org
+          </Link>
+          <Link
+            href="https://amzn.to/3xSaKFJ"
+            target="_blank"
+            className="flex justify-center items-center text-center px-auto text-l p-2 my-4 mr-6 bg-turquoise uppercase rounded-md text-black hover:font-bold"
+          >
+            buy on Amazon
+          </Link>
+        </div>
         <p className="text-sm italic text-center mx-3 mb-3 text-gray-400">
           These are affiliate links, which keep this informational website free
           of charge. Bookshop.org supports our work the most.
